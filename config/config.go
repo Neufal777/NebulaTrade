275bd279b2fb31906a12f213b2c5b52157d22a99
naoufal dahouli <neufal777@gmail.com>
2021-01-06 00:45:47 +0100

    multiple crypto trafding fix

--- config/config.go
+++ config/config.go
@@ -10,3 +10,6 @@ var (
 	CURRENCY = "CURRENCY"
 	COUNTER  = 100 //how much we wait to buy new stock [in seconds] #26 minutes
 )
+
+//go run main.go -currency=MITHBNB -limit=2 -profit=0.00001
+//go run main.go -currency=XLMBNB -limit=2 -profit=0.00001
